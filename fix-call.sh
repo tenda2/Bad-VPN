8985ee412cd44c88aa845e9b080e4c10a97b47c6
tenda2 <azad.arioo@outlook.com>
2023-08-22 00:54:17 +0330

    Update fix-call.sh

--- fix-call.sh
+++ fix-call.sh
@@ -1,33 +1,61 @@
 #!/bin/bash
-clear
-udpport=7300
-echo -e "\nPlease input UDPGW Port ."
-printf "Default Port is \e[33m${udpport}\e[0m, let it blank to use this Port: "
-read udpport
 
+# Update package lists
+sudo apt-get update -y
 
-apt update -y
-apt install git cmake -y
-git clone https://github.com/ambrop72/badvpn.git /root/badvpn
-mkdir /root/badvpn/badvpn-build
-cd  /root/badvpn/badvpn-build
-cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 &
-wait
-make &
-wait
-cp udpgw/badvpn-udpgw /usr/local/bin
-cat >  /etc/systemd/system/videocall.service << ENDOFFILE
+# Upgrade installed packages
+sudo apt-get upgrade -y
+
+# Clean up package cache
+sudo apt-get autoclean -y
+
+# Remove unused packages
+sudo apt-get autoremove -y
+
+# Install 'screen' and 'unzip'
+sudo apt-get install -y screen unzip
+
+# Download badvpn-udpgw
+sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/daybreakersx/premscript/master/badvpn-udpgw64"
+sudo chmod +x /usr/bin/badvpn-udpgw
+
+# Create rc-local.service file
+sudo tee /etc/systemd/system/rc-local.service > /dev/null << EOL
 [Unit]
-Description=UDP forwarding for badvpn-tun2socks
-After=nss-lookup.target
+Description=/etc/rc.local Compatibility
+ConditionPathExists=/etc/rc.local
 
 [Service]
-ExecStart=/usr/local/bin/badvpn-udpgw --loglevel none --listen-addr 127.0.0.1:$udpport --max-clients 999
-User=videocall
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
 
 [Install]
 WantedBy=multi-user.target
-ENDOFFILE
-useradd -m videocall
-systemctl enable videocall
-systemctl start videocall
+EOL
+
+# Make /etc/rc.local executable
+sudo chmod +x /etc/rc.local
+printf '%s\n' '#!/bin/bash' 'exit 0' | sudo tee -a /etc/rc.local
+sudo chmod +x /etc/rc.local
+
+# Enable and start rc-local.service
+sudo systemctl enable rc-local
+sudo systemctl start rc-local.service
+
+# Add content to /etc/rc.local
+sudo tee /etc/rc.local > /dev/null << EOL
+#!/bin/sh -e
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
+exit 0
+EOL
+sudo chmod +x /etc/rc.local
+
+# Display listening ports
+lsof -i -P -n | grep LISTEN
+
+# Reboot the system
+sudo reboot
